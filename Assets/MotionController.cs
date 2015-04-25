aece2b787db101da82fbc66c01312dd662df6ad7
Andriy Oblivansev <eslider@gmail.com>
2015-04-25 20:02:25 +0200

    add MotionController

---
+++ Assets/MotionController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MotionController : MonoBehaviour {
+	public float speedFactor = 0.1f;
+	void Update () {
+		if(Input.GetKey(KeyCode.UpArrow)){
+			transform.Translate(Vector3.forward * speedFactor);
+		}
+		if(Input.GetKey(KeyCode.DownArrow)){
+			transform.Translate(Vector3.back * speedFactor);
+		}
+		if(Input.GetKey(KeyCode.RightArrow)){
+			transform.Translate(Vector3.right * speedFactor);
+		}
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			transform.Translate(Vector3.left * speedFactor);
+		}
+	}
+}
