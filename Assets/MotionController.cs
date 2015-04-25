9400bdbd8dad6225926d8f963231225206124b1d
CasusArts <casusarts@gmail.com>
2015-04-25 21:51:33 +0200

    fix MotionController change logic

--- Assets/MotionController.cs
+++ Assets/MotionController.cs
@@ -1,22 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-// This is MotionController 
-
-public class MotionController : MonoBehaviour {
-	public float speedFactor = 0.1f;
-	void Update () {
-		if(Input.GetKey(KeyCode.UpArrow)){
-			transform.Translate(Vector3.forward * speedFactor);
-		}
-		if(Input.GetKey(KeyCode.DownArrow)){
-			transform.Translate(Vector3.back * speedFactor);
-		}
-		if(Input.GetKey(KeyCode.RightArrow)){
-			transform.Translate(Vector3.right * speedFactor);
-		}
-		if(Input.GetKey(KeyCode.LeftArrow)){
-			transform.Translate(Vector3.left * speedFactor);
-		}
+public class MotionController : MonoBehaviour
+{
+	
+	public float speed = 3;
+	private Rigidbody rb;
+	
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	void Update ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		rb.AddForce (movement * speed);
 	}
 }
