2226210e313a6450708602a063dc99aaa8819d3b
Andriy Oblivansev <eslider@gmail.com>
2015-04-26 03:46:32 +0200

    add change sphere color by wall colliding

--- Assets/MotionController.cs
+++ Assets/MotionController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Text.RegularExpressions;
 
 public class MotionController : MonoBehaviour
 {
@@ -20,5 +21,18 @@ public class MotionController : MonoBehaviour
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		
 		rb.AddForce (movement * speed);
+	}
+
+	void OnCollisionEnter(Collision collision) {
+
+		if(Regex.Match( collision.collider.name, "^Wall").Success){
+			print(collision.collider.name);
+			ContactPoint cp = collision.contacts[0];
+			Renderer renderWall = cp.otherCollider.GetComponent<Renderer>();
+			Renderer renderSphere = cp.thisCollider.gameObject.GetComponent<Renderer>();
+
+			renderSphere.material = renderWall.material;
+		}
+		
 	}
 }
