374e1605d3bd9ba00c26026a31806651d66e3360
CasusArts <casusarts@gmail.com>
2015-04-26 17:52:51 +0200

    add audio sources and samples play sound on collision

---
+++ Assets/CollisionSound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class CollisionSound : MonoBehaviour
+{
+	private AudioSource audiosrc;
+
+	void Start ()
+	{
+		audiosrc = GetComponent<AudioSource> ();
+	}
+
+	void OnCollisionEnter (Collision collision)
+	{
+		audiosrc.Play();
+	}
+}
--- Assets/MotionController.cs
+++ Assets/MotionController.cs
@@ -7,16 +7,21 @@ public class MotionController : MonoBehaviour
 	
 	public float speed = 3;
 	private Rigidbody rb;
+
 	public float additionalColorPercent = 0.000001F;
 	private ContactPoint contactPoint;
 	
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody> ();
+
+//		audiosrc = GetComponent<AudioSource>();
+
 	}
 	
 	void Update ()
 	{
+		// Moving Sphere with keyboard arrows
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 		
@@ -30,13 +35,15 @@ public class MotionController : MonoBehaviour
 			Renderer renderWall = contactPoint.otherCollider.GetComponent<Renderer>();
 			Renderer renderSphere = contactPoint.thisCollider.gameObject.GetComponent<Renderer>();
 
-			print ("change color");
+//			print ("change color");
 			renderSphere.material.color = Color.Lerp(renderSphere.material.color, renderWall.material.color, additionalColorPercent);
+
+
 
 			// if the color is the same, stop changing and set contactPoint = null
 			if(renderWall.material.color == renderSphere.material.color){
 				contactPoint = new ContactPoint();
-				print ("color changed");
+//				print ("color changed");
 			}
 		}
 
@@ -47,7 +54,12 @@ public class MotionController : MonoBehaviour
 		if(Regex.Match( collision.collider.name, "^Wall").Success){
 			print ("start change color");
 			contactPoint = collision.contacts[0];
+
+
 		}
+
 		
 	}
+
+
 }
